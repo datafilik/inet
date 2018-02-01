bdfe78154737ada490332caf8abb1be80dad178a
Zoltan Bojthe <zbojthe@omnetpp.org>
2018-02-01 19:12:33 +0100

    TMP - tests/module - failed tests

--- tests/module/runFailedTests.sh
+++ tests/module/runFailedTests.sh
@@ -1,6 +1,5 @@
 ./runtest \
-    AODVSimpleTest.test IPv4NetworkConfigurator_2.test IPv4NetworkConfigurator_3.test \
-    Ieee80211Retransmission1.test Ieee80211Retransmission2.test Ieee80211Retransmission3.test \
-    Ieee80211Retransmission4.test Ieee80211_1.test Ieee80211_2.test Ieee80211_3.test Ieee80211_4.test \
-    Power_4.test Power_5.test lifecycle_AccessPoint_1.test lifecycle_AccessPoint_2.test \
-    lifecycle_AccessPoint_3.test lifecycle_WirelessHost_4.test lifecycle_WirelessHost_5.test
+    IPv4_refragmentation.test IPv6_fragmentation.test IPvXTrafGen_lifecycle.test \
+    UDPSocket_1.test UDP_tos_ipv4.test UDP_tos_ipv6.test UDP_ttl_ipv4.test UDP_ttl_ipv6.test \
+    tcp_algorithm_dumb.test tcp_algorithm_newreno.test tcp_algorithm_reno.test tcp_algorithm_tahoe.test \
+    tcp_algorithm_vegas.test tcp_algorithm_westwood.test
